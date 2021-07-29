37a0ac17c33fa7a620d5dfbd3a8f0d21e060fcb2
ninahorne <nina.horne1221@gmail.com>
2021-07-29 09:40:12 -0400

    Students page | eligibility card mobile and desktop

--- wp-content/themes/bor/students.php
+++ wp-content/themes/bor/students.php
@@ -41,12 +41,297 @@
     </section>
     <section class="students-page__enrollment-steps">
       <div class="container">
-      <?php include('enrollment-steps.php'); ?>
+        <?php include('enrollment-steps.php'); ?>
 
       </div>
 
     </section>
+    <section class="students-page__eligibility">
+      <!-- <img class="background-brush-stroke" src="<?php echo get_template_directory_uri(); ?>/images/large-yellow-brush-stroke.svg" alt=""> -->
 
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-lg-5">
+            <h1>Eligibility</h1>
+            <p>
+              Dual Enrollment is very accessible. First, a 2.5 cumulative GPA is required. Then, either a subject-specific test score OR a recommendation from their counselor. To see the specific test score requirements for non-technical subjects, visit the FAQ page. <br />
+
+              <br />
+              The technical course options often require tools, so they have an age limit (15+) and require good standing in high school. Each college has their own paperwork requirements, which can be found using the useful college links resource.
+            </p>
+          </div>
+          <div class="col-md-8 col-lg-7">
+
+            <div class="student-eligibility__white-card d-none d-md-block">
+              <div class="row">
+                <div class="col-md-6">
+                  <h3>Non-technial Courses</h3>
+
+                </div>
+                <div class="col-md-6">
+                  <h3>Technical Courses</h3>
+                </div>
+
+                <div class="col-md-6">
+                  <div class="student-eligibility__white-card__item">
+                    <div class="row">
+                      <div class="col-2">
+                        <i class="far fa-check-circle"></i>
+
+                      </div>
+                      <div class="col-10">
+                        <h5>Required: 2.5 Cumulative GPA or above</h5>
+
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+                <div class="col-md-6">
+                  <div class="student-eligibility__white-card__item">
+                    <div class="row">
+                      <div class="col-2">
+                        <i class="far fa-check-circle"></i>
+
+                      </div>
+                      <div class="col-10">
+                        <h5>Required: 2.0 Cumulative GPA or above</h5>
+
+                      </div>
+                    </div>
+
+
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="student-eligibility__white-card__item">
+                    <div class="row">
+                      <div class="col-2">
+                        <i class="far fa-check-circle"></i>
+
+                      </div>
+                      <div class="col-10">
+                        <h5>Required: All paperwork (see college links)</h5>
+
+                      </div>
+                    </div>
+
+
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="student-eligibility__white-card__item">
+                    <div class="row">
+                      <div class="col-2">
+                        <i class="far fa-check-circle"></i>
+
+                      </div>
+                      <div class="col-10">
+                        <h5>Required: Age 15 and up (younger by exception)</h5>
+
+                      </div>
+                    </div>
+
+
+                  </div>
+                </div>
+
+                <div class="col-md-6">
+                  <div class="student-eligibility__white-card__item">
+                    <div class="row">
+                      <div class="col-2">
+                        <i class="far fa-check-circle"></i>
+
+                      </div>
+                      <div class="col-10">
+                        <h5>Option 1: Demonstrated mastery on standardized test</h5>
+
+                      </div>
+                    </div>
+
+
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="student-eligibility__white-card__item">
+                    <div class="row">
+                      <div class="col-2">
+                        <i class="far fa-check-circle"></i>
+
+                      </div>
+                      <div class="col-10">
+                        <h5>Required: All paperwork (see college links)</h5>
+
+                      </div>
+                    </div>
+
+
+                  </div>
+                </div>
+
+                <div class="col-md-6">
+                  <div class="student-eligibility__white-card__item">
+                    <div class="row">
+                      <div class="col-2">
+                        <i class="far fa-check-circle"></i>
+
+                      </div>
+                      <div class="col-10">
+                        <h5>Option 2: Counselor recommendation</h5>
+
+                      </div>
+                    </div>
+
+
+                  </div>
+                </div>
+                <div class="col-md-6">
+
+
+                  <div class="student-eligibility__white-card__item">
+                    <div class="row">
+                      <div class="col-2">
+                        <i class="far fa-check-circle"></i>
+
+                      </div>
+                      <div class="col-10">
+                        <h5>Sometimes: Safety standards</h5>
+
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+
+
+
+              </div>
+            </div>
+
+
+            <div class="student-eligibility__white-card d-block d-md-none">
+              <div class="row">
+                <div>
+                  <h3>Non-technial Courses</h3>
+                  <div class="student-eligibility__white-card__item">
+                    <div class="row">
+                      <div class="col-2">
+                        <i class="far fa-check-circle"></i>
+
+                      </div>
+                      <div class="col-10">
+                        <h5>Required: 2.5 Cumulative GPA or above</h5>
+
+                      </div>
+                    </div>
+                  </div>
+                  <div class="student-eligibility__white-card__item">
+                    <div class="row">
+                      <div class="col-2">
+                        <i class="far fa-check-circle"></i>
+
+                      </div>
+                      <div class="col-10">
+                        <h5>Required: All paperwork (see college links)</h5>
+
+                      </div>
+                    </div>
+
+
+                  </div>
+                  <div class="student-eligibility__white-card__item">
+                    <div class="row">
+                      <div class="col-2">
+                        <i class="far fa-check-circle"></i>
+
+                      </div>
+                      <div class="col-10">
+                        <h5>Option 1: Demonstrated mastery on standardized test</h5>
+
+                      </div>
+                    </div>
+
+
+                  </div>
+                  <div class="student-eligibility__white-card__item">
+                    <div class="row">
+                      <div class="col-2">
+                        <i class="far fa-check-circle"></i>
+
+                      </div>
+                      <div class="col-10">
+                        <h5>Option 2: Counselor recommendation</h5>
+
+                      </div>
+                    </div>
+
+
+                  </div>
+                </div>
+                <div>
+                  <h3>Technical Courses</h3>
+                  <div class="student-eligibility__white-card__item">
+                    <div class="row">
+                      <div class="col-2">
+                        <i class="far fa-check-circle"></i>
+
+                      </div>
+                      <div class="col-10">
+                        <h5>Required: 2.0 Cumulative GPA or above</h5>
+
+                      </div>
+                    </div>
+
+
+                  </div>
+                  <div class="student-eligibility__white-card__item">
+                    <div class="row">
+                      <div class="col-2">
+                        <i class="far fa-check-circle"></i>
+
+                      </div>
+                      <div class="col-10">
+                        <h5>Required: Age 15 and up (younger by exception)</h5>
+
+                      </div>
+                    </div>
+
+
+                  </div>
+                  <div class="student-eligibility__white-card__item">
+                    <div class="row">
+                      <div class="col-2">
+                        <i class="far fa-check-circle"></i>
+
+                      </div>
+                      <div class="col-10">
+                        <h5>Required: All paperwork (see college links)</h5>
+
+                      </div>
+                    </div>
+
+
+                  </div>
+                  <div class="student-eligibility__white-card__item">
+                    <div class="row">
+                      <div class="col-2">
+                        <i class="far fa-check-circle"></i>
+
+                      </div>
+                      <div class="col-10">
+                        <h5>Sometimes: Safety standards</h5>
+
+                      </div>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </div>
 
