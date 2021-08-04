77f8f050269244f53f58ec6297faa31552bdf047
ninahorne <nina.horne1221@gmail.com>
2021-08-04 10:50:33 -0400

    Families page | blue section - desktop - mobile

--- wp-content/themes/bor/parents.php
+++ wp-content/themes/bor/parents.php
@@ -1,12 +1,36 @@
 <?php
-    /**
-    * Template Name: Parents Page
-    */
+
+/**
+ * Template Name: Families Page
+ */
 ?>
 
 <?php include('header.php'); ?>
 
+<div id="content">
+    <div id="families">
+        <div class="families-page__banner">
+            <div class="flex-vertical-center">
+                <div class="container">
+                    <h1>Information for Families</h1>
 
+                </div>
+            </div>
+        </div>
+        <section class="families-page__section-one">
+            <div class="flex-vertical-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-7">
+                            <h1>Let’s help your student experience college early.</h1>
+                            <p>With dual enrollment, students may begin accumulating college credits while still in high school, thus providing a smoother transition to college after high school graduation. Students also have the opportunity to complete college faster by earning college credits while still in high school.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+</div>
 
 
 <?php include('footer.php'); ?>
