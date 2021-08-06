57a73e347f86850eb932ed98d222e47cc9222e53
ninahorne <nina.horne1221@gmail.com>
2021-08-06 10:14:44 -0400

    Reformatted Useful College Links results

--- wp-content/themes/bor/get_college.php
+++ wp-content/themes/bor/get_college.php
@@ -18,7 +18,10 @@ $sql = "SELECT * FROM `wp_postmeta` WHERE post_id=$post_id";
 $college_link = $conn->query($sql);
 $system;
 $campus;
-$registrarName;
+$departmentContactName;
+$departmentContactEmail;
+$dualEnrollmentApplication;
+$transferForm;
 $registrarEmail;
 
 if ($college_link->num_rows > 0) {
@@ -33,23 +36,44 @@ if ($college_link->num_rows > 0) {
         if ($row['meta_key'] == 'campus') {
             $campus = $row['meta_value'];
         }
-        if ($row['meta_key'] == 'registrar_name') {
-            $registrarName = $row['meta_value'];
+        if ($row['meta_key'] == 'department_contact_name_') {
+            $departmentContactName = $row['meta_value'];
+        }
+        if ($row['meta_key'] == 'department_contact_email') {
+            $departmentContactEmail = $row['meta_value'];
+        }
+        if ($row['meta_key'] == 'duel_enrollment_application') {
+            $dualEnrollmentApplication = $row['meta_value'];
+        }
+        if ($row['meta_key'] == 'transfer_form') {
+            $transferForm = $row['meta_value'];
         }
         if ($row['meta_key'] == 'registrar_contact_information') {
             $registrarEmail = $row['meta_value'];
         }
-
-
     }
 
     echo '<div class="useful-college-links__selected-result">
-    <i onclick="clearSelectedCollege()" class="far fa-times-circle"></i>
-    <div class="row"><div class="col-md-5"><h2>' .
-        $system . '</h2><p>' . $campus . '</p></div><div class="col-md-7"><h3>Registrar</h3>
-    <div class="row"><div class="col-md-6"><p>' . $registrarName . '</p><p>' . $registrarEmail . '</p></div>' .
-            '<div class="col-md-6"><p><i class="fas fa-external-link-alt"></i>&nbsp;Request your transcript</p><p><i class="fas fa-external-link-alt"></i>&nbsp;Contact the DE coordinator </p></div>' . '
-    </div>';
+            <i onclick="clearSelectedCollege()" class="far fa-times-circle"></i>
+            <div class="row">
+                <div class="col-md-5">
+                    <h2>' . $campus . '</h2>
+                    <a target="_blank" class="unformatted" href="' . $dualEnrollmentApplication . '"><i class="fas fa-external-link-alt"></i>&nbsp;Go to Dual Enrollment Application</a>
+
+                </div>
+                <div class="col-md-7">
+                    <h3>Dual Enrollment Contact</h3>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <p>' . $departmentContactName . '</p>
+                            <a class="unformatted" href="mailto:' . $departmentContactEmail . '"><i class="far fa-envelope"></i>&nbsp;&nbsp;' . $departmentContactEmail . '</a>
+                        </div>' .
+        '           <div class="col-lg-6">
+                            <a target="_blank" class="d-block unformatted" href="' . $transferForm . '"><i class="fas fa-external-link-alt"></i>&nbsp;Request your transcript</a>'
+                            . (isset($registrarEmail) ? '<a class="unformatted" href="mailto:' . $registrarEmail . '"><i class="far fa-envelope"></i>&nbsp;&nbsp;Contact the Registrar </a>' : '') . '
+                            
+                        </div>' . '
+            </div>';
 } else {
     echo "0 results";
 }
